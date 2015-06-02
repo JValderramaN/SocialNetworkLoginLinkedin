d0e996580722048d60de3e0b190bdbed4df8788b
Robert Bevilacqua <RBevilacqua11@gmail.com>
2015-06-01 23:09:39 -0430

    added comment

--- LinkedIn/ViewController.swift
+++ LinkedIn/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 import OAuthSwift
 
+/**
+*   The premise of Oauth is declared
+*/
+
 let oauthswift = OAuth2Swift(
     consumerKey:    Linkedin["consumerKey"]!,
     consumerSecret: Linkedin["consumerSecret"]!,
@@ -26,26 +30,40 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    /**
+    *   In this function, var oautswift handles request authorization API LinkedIn
+    */
     func doOAuthLinkedin(){
         
         let state: String = generateStateWithLength(20) as String
         oauthswift.authorizeWithCallbackURL( NSURL(string: "http://oauthswift.herokuapp.com/callback/linkedin2")!, scope: "r_basicprofile", state: state, success: {
             credential, response in
+            /**
+            *   Here in Alert, you can get the LinkedIn token through credential.
+            */
             //self.showAlertView("LinkedIn", message: "oauth_token:\(credential.oauth_token)")
+            /**
+            *   Is presented the secondViewController
+            */
             var vc = UIStoryboard(name: "ViewController", bundle: nil).instantiateViewControllerWithIdentifier("login") as! UIViewController
             self.presentViewController(vc, animated: true, completion: nil)
             }, failure: {(error:NSError!) -> Void in
                 println(error.localizedDescription)
         })
     }
-
+    
+    /**
+    *   This function generates the alert
+    */
     func showAlertView(title: String, message: String) {
         var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
         alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Default, handler: nil))
         self.presentViewController(alert, animated: true, completion: nil)
     }
     
+    /**
+    *   When you press the login button with LinkedIn, runs request authorization API LinkedIn
+    */
     @IBAction func loginLinkedIn(sender: UIButton) {
         doOAuthLinkedin()
     }
